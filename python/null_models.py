0973a160987674b06426a58f893a403c90405bb8
Timothy LaRock <timothylarock@gmail.com>
2023-12-21 18:54:55 -0500

    Added 2nd day of christmas.

--- python/null_models.py
+++ python/null_models.py
@@ -31,6 +31,79 @@ def simulate_chaos(G, T, output_dir):
         file_path = os.path.join(output_dir, f'adj_matrix_{t}.csv')
         adjacency_matrix.to_csv(file_path)
 
+
+def simulate_exponential(G, T, decay_factor, output_dir, arrival_type="uniform"):
+    """
+    arrival (bool): If true, use exponential arrival. Otherwise use exponential
+    departure.
+    """
+    rng = np.random.default_rng()
+
+    if arrival_type == "uniform":
+        # Exponential Departure case
+        # Choose arrival times uniformly at random on 1,T
+        arrival_times = rng.integers(low=1, high=T, size=G.number_of_nodes())
+    else:
+        # Exponential arrival case
+        duration = rng.integers(low=1, high=int(T/2.0), size=G.number_of_nodes())
+
+    # Simulate and save adjacency matrices
+    node_age = np.zeros(G.number_of_nodes())
+    active_nodes = set()
+    inactive_nodes = list(G.nodes())
+    departed_nodes = set()
+    for t in range(T):
+        probabilities = rng.uniform(size=len(inactive_nodes))
+        if arrival_type == "uniform":
+            # Compute decay probabilites for each node based on their age
+            decay = np.array([np.exp(-decay_factor * (node_age[node]+1)) for node in range(node_age.shape[0])])
+
+            # Check whether nodes need to be added to the network
+            for node in inactive_nodes:
+                if t > arrival_times[node]:
+                    active_nodes.add(node)
+                    node_age[node] = 1
+
+            # The nodes with probability less than decay will be removed
+            departed_nodes = set(np.where(probabilities < decay)[0])
+            for node in active_nodes:
+                if node not in departed_nodes:
+                    # if not removed, increment age
+                    node_age[node]+=1
+            # Actually remove departing nodes
+            active_nodes -= departed_nodes
+        else:
+            # Same decay for every still-inactive node
+            decay = np.exp(-decay_factor * (t+1))
+            new_nodes = np.where(probabilities < decay)[0]
+
+            # Add the new nodes
+            for node in new_nodes:
+                active_nodes.add(node)
+                node_age[node] = 1
+
+            for node in active_nodes:
+                # Remove a node if its age is larger than the duration we
+                # assigned to it
+                if node_age[node] > duration[node]:
+                    departed_nodes.add(node)
+                else:
+                    node_age[node] += 1
+
+            active_nodes -= departed_nodes
+
+        subgraph = nx.DiGraph(G.subgraph(active_nodes))
+        # In the output we want all of the nodes present, 
+        # so make sure they are all there
+        for node in G.nodes():
+            if node not in subgraph:
+                subgraph.add_node(node)
+
+        adjacency_matrix = nx.to_pandas_adjacency(subgraph)
+        file_path = os.path.join(output_dir, f'adj_matrix_{t}.csv')
+        adjacency_matrix.to_csv(file_path)
+
+
 if __name__ == "__main__":
     # Parameters
     num_species = 25
@@ -52,4 +125,7 @@ if __name__ == "__main__":
     adjacency_matrix.to_csv(file_path)
 
     # Run the simulation and save the outputs
-    simulate_chaos(interaction_graph, T, output_dir)
+    #simulate_chaos(interaction_graph, T, output_dir)
+    decay_factor = 0.8
+    simulate_exponential(interaction_graph, T, decay_factor, output_dir,
+                         arrival_type="exp")
