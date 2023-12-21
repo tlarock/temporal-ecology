6591c76ae5d32b12d97d8b9cb08e0ce2c532ab15
Timothy LaRock <timothylarock@gmail.com>
2023-12-21 15:57:12 -0500

    Separated trophic network generation from original interaction network model.

--- python/trophic_network.py
+++ python/trophic_network.py
@@ -1,61 +1,27 @@
 import numpy as np
 import networkx as nx
-from trophic_network import get_species_mass
 
-
-def get_intervals(num_species,
-                  arrival_mu, arrival_var,
-                  interval_mu, interval_var):
+def get_species_mass(num_species, alpha, beta):
     """
-        Constructs temporal activity intervals for
-        num_species species with arrival times and
-        intervals drawn from a normal distribution.
+        Draws masses for num_species species using
+        a Beta distribution with shape parameters
+        alpha and beta
 
         Parameters
         ----------
         num_species (int): Number of species
-        arrival_mu, arrival_var (float): Mean and variance for the arrival time normal distribution
-        interval_mu, interval_var (float): Mean and variance for the interval normal distribution
+        alpha, beta (float): Shape parameter
 
         Returns
         ----------
-        intervals (np.array): 2d array of intervals where the first entry is the arrival time
-        and the second entry is the departure time
+        masses (np.array): Array of length num_species
+                            containing the sampled masses
+
     """
-    rng = np.random.default_rng()
-    arrivals = rng.normal(arrival_mu, arrival_var, size=num_species)
-    durations = rng.normal(interval_mu, interval_var, size=num_species)
-    departures = arrivals + durations
-    return np.vstack((arrivals, departures))
+    return np.random.beta(alpha, beta, num_species)
 
 
-def compute_overlaps(intervals):
-    """
-        Compute the overlap between all pairs of intervals and store in a
-        dictionary indexed by tuples (i,j) where i<j
-
-        Parameters
-        ----------
-        intervals (np.array): 2d array of intervals where the first entry is the arrival time
-        and the second entry is the departure time
-
-        Returns
-        ----------
-        overlaps (dict): A dictionary from tuples (i,j) of pairs of species
-        with i<j to the absolute overlap between the temporal intervals of the
-        two species.
-    """
-    overlaps = dict()
-    for i in range(intervals.shape[1]):
-        for j in range(i+1, intervals.shape[1]):
-            a = intervals[:, i]
-            b = intervals[:, j]
-            overlap = max(0, min(a[1], b[1]) - max(a[0], b[0]))
-            overlaps[(i,j)] = overlap
-    return overlaps
-
-
-def generate_interactions_from_intervals(species_mass, intervals, C, max_attempts=100):
+def generate_network(species_mass, C, max_attempts=100):
     """
         Constructs interactions between species
         based on their biomass/niche and temporal overlap
@@ -78,9 +44,6 @@ def generate_interactions_from_intervals(species_mass, intervals, C, max_attempt
     # Setup
     rng = np.random.default_rng()
     num_species = species_mass.shape[0]
-    # Get the overlap between time intervals
-    # Note: These are unnormalized
-    overlaps = compute_overlaps(intervals)
 
     # This is the beta probability we will use, which
     # is based on the desired network density C (input)
@@ -118,16 +81,8 @@ def generate_interactions_from_intervals(species_mass, intervals, C, max_attempt
                 # Check if the mass of the prey is in the range
                 # we have assigned for the predator
                 if low[pred] < species_mass[prey] < high[pred]:
-                    # We will store the overlap as a proportion
-                    # of the predator interval
-                    norm = intervals[1, pred] - intervals[0, pred]
-                    if pred < prey:
-                        weight = overlaps[(pred, prey)] / norm
-                    else:
-                        weight = overlaps[(prey, pred)] / norm
-
                     # Add the edge to the networkx graph
-                    G.add_edge(prey, pred, weight=weight)
+                    G.add_edge(prey, pred)
 
         # Check whether all of the species have non-zero degree
         interacting_species = len(set([node for node in G if G.degree(node, weight="weight") > 0]))
