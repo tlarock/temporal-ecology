4af35201ca3949e551bb4d8ecc10018ae11824db
Timothy LaRock <timothylarock@gmail.com>
2023-12-22 11:38:44 -0500

    Fixed bug in tracking inactive nodes.

--- python/null_models.py
+++ python/null_models.py
@@ -38,10 +38,12 @@ def sigmoid(x, k):
     """
     return (x - k*x) / (k - 2*k*abs(x) + 1)
 
-def simulate_exponential(G, T, decay_factor, output_dir,
+def simulate_temporal_network(G, T, decay_factor, output_dir,
                          arrival_type="uniform", departure_type="uniform",
                         sigmoid_thresh=0.5):
     """
+
+
     arrival (bool): If true, use exponential arrival. Otherwise use exponential
     departure.
     """
@@ -63,97 +65,101 @@ def simulate_exponential(G, T, decay_factor, output_dir,
     active_nodes = set()
     inactive_nodes = list(G.nodes())
     departed_nodes = set()
-    for t in range(T):
-        probabilities = rng.uniform(size=len(inactive_nodes))
-        # Manage node arrivals
-        if arrival_type == "uniform":
-            # Check whether nodes need to be added to the network
-            for node in inactive_nodes:
-                if t > arrival_times[node]:
+    for t in range(1, T+1):
+        if len(inactive_nodes) > 0:
+            probabilities = rng.uniform(size=len(inactive_nodes))
+            # Manage node arrivals
+            if arrival_type == "uniform":
+                # Check whether nodes need to be added to the network
+                for node in inactive_nodes:
+                    if t > arrival_times[node]:
+                        active_nodes.add(node)
+                        node_age[node] = 1
+            elif arrival_type == "exponential":
+                # Same decay for every still-inactive node
+                decay = np.exp(-decay_factor * (t+1))
+                new_nodes = np.where(probabilities < decay)[0]
+
+                # Add the new nodes
+                for node in new_nodes:
                     active_nodes.add(node)
                     node_age[node] = 1
-        elif arrival_type == "exponential":
-            # Same decay for every still-inactive node
-            decay = np.exp(-decay_factor * (t+1))
-            new_nodes = np.where(probabilities < decay)[0]
+            elif arrival_type == "sigmoid":
+                entrance_probs = []
+                # Update node probabilities with sigmoid function
+                for node in inactive_nodes:
+                    predecessors = [n for n in G.predecessors(node) if n in active_nodes]
+                    total_predecessors = len(list(G.predecessors(node)))
+                    proportion = len(predecessors) / total_predecessors if total_predecessors > 0 else 0
+                    if proportion >= sigmoid_thresh:
+                        k = (proportion-0.5)*2
+                    else:
+                        k = -(proportion-0.5)*2
 
-            # Add the new nodes
-            for node in new_nodes:
-                active_nodes.add(node)
-                node_age[node] = 1
-        elif arrival_type == "sigmoid":
-            entrance_probs = np.zeros(G.number_of_nodes())
-            # Update node probabilities with sigmoid function
-            for node in G.nodes():
-                predecessors = [n for n in G.predecessors(node) if n in inactive_nodes]
-                total_predecessors = len(list(G.predecessors(node)))
-                proportion = len(predecessors) / total_predecessors if total_predecessors > 0 else 0
-                if proportion >= sigmoid_thresh:
-                    k = (proportion-0.5)*2
-                else:
-                    k = -(proportion-0.5)*2
+                    if k == 1.0:
+                        k -= 0.0001
 
-                if k == 1.0:
-                    k -= 0.0001
+                    entrance_probs.append(sigmoid(t+1, k))
 
-                entrance_probs[node] = sigmoid(t+1, k)
-
-            entrance_probs /= entrance_probs.sum()
-            new_nodes = np.where(entrance_probs < probabilities)[0]
-            for node in new_nodes:
-                active_nodes.add(node)
-                node_age[node] = 1
+                entrance_probs = np.array(entrance_probs)
+                entrance_probs /= entrance_probs.sum()
+                new_nodes = np.where(entrance_probs < probabilities)[0]
+                for node in new_nodes:
+                    active_nodes.add(node)
+                    node_age[node] = 1
 
 
         # Manage node departures
-        probabilities = rng.uniform(size=len(active_nodes))
-        if departure_type == "exponential":
-            # Compute decay probabilites for each node based on their age
-            decay = np.array([np.exp(-decay_factor * (node_age[node]+1)) for
-                              node in range(node_age.shape[0]) if node in
-                              active_nodes])
-            # The nodes with probability less than decay will be removed
-            departed_nodes = set(np.where(probabilities < decay)[0])
-            for node in active_nodes:
-                if node not in departed_nodes:
-                    # if not removed, increment age
-                    node_age[node]+=1
-        elif departure_type == "uniform":
-            for node in active_nodes:
-                # Remove a node if its age is larger than the duration we
-                # assigned to it
-                if node_age[node] > duration[node]:
-                    departed_nodes.add(node)
-                else:
-                    node_age[node] += 1
-        elif departure_type == "sigmoid":
-            departure_probs = []
-            # Update node probabilities with sigmoid function
-            for node in active_nodes:
-                predecessors = [n for n in G.predecessors(node) if n in active_nodes]
-                total_predecessors = len(list(G.predecessors(node)))
-                proportion = len(predecessors) / total_predecessors if total_predecessors > 0 else 0
-                if proportion >= sigmoid_thresh:
-                    k = (proportion-0.5)*2
-                else:
-                    k = -(proportion-0.5)*2
+        if len(active_nodes) > 0:
+            probabilities = rng.uniform(size=len(active_nodes))
+            if departure_type == "uniform":
+                for node in active_nodes:
+                    # Remove a node if its age is larger than the duration we
+                    # assigned to it
+                    if node_age[node] > duration[node]:
+                        departed_nodes.add(node)
+                    else:
+                        node_age[node] += 1
 
-                if k == 1.0:
-                    k -= 0.0001
+            elif departure_type == "exponential":
+                # Compute decay probabilites for each node based on their age
+                decay = np.array([np.exp(-decay_factor * (node_age[node]+1)) for
+                                  node in range(node_age.shape[0]) if node in
+                                  active_nodes])
+                # The nodes with probability less than decay will be removed
+                departed_nodes = set(np.where(probabilities < decay)[0])
+                for node in active_nodes:
+                    if node not in departed_nodes:
+                        # if not removed, increment age
+                        node_age[node]+=1
+            elif departure_type == "sigmoid":
+                departure_probs = []
+                # Update node probabilities with sigmoid function
+                for node in active_nodes:
+                    predecessors = [n for n in G.predecessors(node) if n in active_nodes]
+                    total_predecessors = len(list(G.predecessors(node)))
+                    proportion = len(predecessors) / total_predecessors if total_predecessors > 0 else 0
+                    if proportion >= sigmoid_thresh:
+                        k = (proportion-0.5)*2
+                    else:
+                        k = -(proportion-0.5)*2
 
-                departure_probs.append(sigmoid(node_age[node], k))
+                    if k == 1.0:
+                        k -= 0.0001
 
-            departure_probs = np.array(departure_probs)
-            departure_probs /= departure_probs.sum()
-            departed_nodes = set(np.where(probabilities < departure_probs)[0])
-            for node in active_nodes:
-                if node not in departed_nodes:
-                    # if not removed, increment age
-                    node_age[node]+=1
+                    departure_probs.append(sigmoid(node_age[node], k))
+
+                departure_probs = np.array(departure_probs)
+                departure_probs /= departure_probs.sum()
+                departed_nodes = set(np.where(probabilities < departure_probs)[0])
+                for node in active_nodes:
+                    if node not in departed_nodes:
+                        # if not removed, increment age
+                        node_age[node]+=1
 
         # Actually remove departing nodes
         active_nodes -= departed_nodes
-
+        inactive_nodes = [node for node in inactive_nodes if node not in active_nodes]
         # Construct the subgraph from active nodes
         subgraph = nx.DiGraph(G.subgraph(active_nodes))
         # In the output we want all of the nodes present, 
@@ -191,5 +197,5 @@ if __name__ == "__main__":
             file_path = output_dir + f'_adj_matrix_full'
             np.save(file_path, adjacency_matrix)
             print(arrival_type, departure_type)
-            simulate_exponential(interaction_graph, T, decay_factor, output_dir,
+            simulate_temporal_network(interaction_graph, T, decay_factor, output_dir,
                                  arrival_type=arrival_type, departure_type=departure_type)
